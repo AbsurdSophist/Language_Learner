51c56503dd01b500bc1894528565c6d6f3274847
Lucas Manning <60553699+AbsurdSophist@users.noreply.github.com>
2024-01-08 19:29:24 -0700

    Initial commit with course files and completion of first activity

---
+++ JavaScript/Udemy/01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,9 @@
+/*
+let country = 'United States';
+let continent = 'North America';
+let population = 329.5;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+*/
---
+++ JavaScript/Udemy/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,27 @@
+/*
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log('Jonas');
+console.log(23);
+
+let firstName = 'Jonas';
+console.log(firstName);
+
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 3);
+
+// 'let' not necessary if we are reassigning a value to a variable
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+*/
+
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
