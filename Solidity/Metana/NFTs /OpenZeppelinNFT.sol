2d1b882e8718829d86fb5fae22b4e5b0eeb6d9cd
Lucas Manning <60553699+AbsurdSophist@users.noreply.github.com>
2024-03-16 19:28:43 -0600

    openzeppelinnft upload adding baseuri funciton

--- Solidity/Metana/NFTs /OpenZeppelinNFT.sol
+++ Solidity/Metana/NFTs /OpenZeppelinNFT.sol
@@ -8,17 +8,23 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract OpenZeppelinNFT is ERC721, Ownable {
     uint256 public tokenSupply = 0;
     uint256 public constant MAX_SUPPLY = 5;
-    uint256 public constant PRICE = 1 ether;
+    uint256 public constant PRICE = 0.001 ether;
     address immutable deployer;
 
     constructor() Ownable(msg.sender) ERC721("MyNFT", "MN") {
         deployer = msg.sender;
     }
 
-    function mint() external {
+    function mint() external payable {
         require(tokenSupply < MAX_SUPPLY, "Max supply met");
+        require(msg.value == PRICE, "Wrong price");
+
         _mint(msg.sender, tokenSupply);
         tokenSupply++;
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "";
     }
 
     function viewBalance() external view returns (uint256) {
