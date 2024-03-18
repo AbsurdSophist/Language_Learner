88e1b90a82f610bd26078c27f098ff0c899753b4
Lucas Manning <60553699+AbsurdSophist@users.noreply.github.com>
2024-03-17 20:07:41 -0600

    added functionality for contract to receive nft

--- Solidity/Metana/NFTs /NFTStaking.sol
+++ Solidity/Metana/NFTs /NFTStaking.sol
@@ -12,10 +12,31 @@ contract Item is ERC721 {
     }
 }
 
-contract Game {
+contract Game is IERC721Receiver {
     IERC721 public itemNFT;
+
+    mapping(uint256 => address) public originalOwner;
 
     constructor(IERC721 _address) {
         itemNFT = _address;
+    }
+
+    function onERC721Received(
+        address operator,
+        address from,
+        uint256 tokenId,
+        bytes calldata data
+    ) external returns (bytes4) {
+        return IERC721Receiver.onERC721Received.selector;
+    }
+
+    function depositNFT(uint256 tokenID) external {
+        originalOwner[tokenID] = msg.sender;
+        itemNFT.safeTransferFrom(msg.sender, address(this), tokenID);
+    }
+
+    function withdrawNFT(uint tokenID) external {
+        require(originalOwner[tokenID] == msg.sender, "Not original owner");
+        itemNFT.safeTransferFrom(address(this), msg.sender, tokenID);
     }
 }
