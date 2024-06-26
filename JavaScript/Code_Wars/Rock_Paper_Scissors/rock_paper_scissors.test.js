6a918b95d6b7402c449fbb091cd883f08bacc242
Lucas Manning <60553699+AbsurdSophist@users.noreply.github.com>
2024-06-26 00:03:03 -0600

    Spelling correction

--- JavaScript/Code_Wars/Rock_Paper_Scissors/rock_paper_scissors.test.js
+++ JavaScript/Code_Wars/Rock_Paper_Scissors/rock_paper_scissors.test.js
@@ -1,4 +1,4 @@
-// Chalenge: You have to return which player won! In case of a draw return Draw!
+// Challenge: You have to return which player won! In case of a draw return Draw!
 
 const rps = (p1, p2) => {
     if (p1 === p2) return 'Draw!';
